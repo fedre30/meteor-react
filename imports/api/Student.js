a020c8ed155f378fb6fdd8c2588c52c2cb19a55f
Federica Alfano <alfanofederica95@gmail.com>
2019-01-14 10:20:10 +0100

    Fix on list

--- imports/api/Student.js
+++ imports/api/Student.js
@@ -1,19 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import list from 'List';
+
 
 
 export const Student = {
 
 };
 
-Meteor.startup(() => {
-  if( list.find().count() <= 0 )
-  {
-    Student.insert(
-      {
-        name : "Federica Alfano"
-      }
-    );
-  }
-});
+
 
